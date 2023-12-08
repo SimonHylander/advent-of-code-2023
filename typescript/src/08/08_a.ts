7a7a302f0ceaa19f1c5ac2eba89517e2b60b8bac
Simon Hylander <hylandersimon@gmail.com>
2023-12-08 12:51:13 +0100

    wip 08

--- typescript/src/08/08_a.ts
+++ typescript/src/08/08_a.ts
@@ -1,12 +1,17 @@
+import { ListFormat } from "typescript";
+
 async function run() {
   const input = await Bun.file(
     new URL(`${import.meta.url}/../input.txt`)
   ).text();
-  const lines = input.split("\n");
-  const rl = lines.shift()?.split("") as string[];
+
+  const lines = [...input.split("\n")];
+  let directions = input.split("\n").shift()?.split("") as string[];
+  console.log(lines);
 
   const network = lines.slice(1, lines.length).map((line, i) => {
-    const [element, instructions] = line.split(" = ");
+    console.log(line);
+    /* const [element, instructions] = line.split(" = ");
     const next = instructions
       .substring(instructions.indexOf("(") + 1, instructions.lastIndexOf(")"))
       .trim()
@@ -15,8 +20,15 @@ async function run() {
     return {
       element,
       next,
-    };
+    }; */
+
+    return 0;
   });
+
+  return;
+
+  // let directions = resetDirections();
+
   // console.log(rl.length);
   // console.log(network);
 
@@ -24,38 +36,60 @@ async function run() {
   let steps = 0;
 
   while (currentNode.element !== "ZZZ") {
-    for (const direction of rl) {
-      if (direction === "L") {
-        const [left, right] = currentNode.next;
+    // console.log(directions);
+    console.log(directions.length);
 
-        const next = network.find((n) => n.element === left);
-        if (next) {
-          currentNode = next
-          steps++
-          
-          if (currentNode.element === "ZZZ") {
-            console.log("reached")
-            break
-          }
-        }
+    if (directions.length === 0) {
+      console.log(resetDirections());
+      // directions = resetDirections();
+      break;
+    }
+
+    if (!directions) {
+      // console.log(currentNode);
+      // break
+    }
+
+    const direction = directions.shift();
+    // console.log(direction);
+
+    if (direction === "L") {
+      const [left, right] = currentNode.next;
+      if (left === "ZZZ") {
+        console.log(left);
       }
 
-      if (direction === "R") {
-        const [left, right] = currentNode.next;
-        const next = network.find((n) => n.element === right);
-        if (next) {
-          currentNode = next;
-          steps++
+      const next = network.find((n) => n.element === left);
+      if (next) {
+        currentNode = next;
+        steps++;
 
-          if (currentNode.element === "ZZZ") {
-            break;
-          }
+        if (currentNode.element === "ZZZ") {
+          console.log("reached");
+          break;
+        }
+      }
+    }
+
+    if (direction === "R") {
+      const [left, right] = currentNode.next;
+      const next = network.find((n) => n.element === right);
+      if (next) {
+        currentNode = next;
+        steps++;
+
+        if (currentNode.element === "ZZZ") {
+          break;
         }
       }
     }
   }
 
   console.log(steps);
+
+  function resetDirections() {
+    return input.split("\n").shift()?.split("") as string[];
+  }
 }
 
 run();
