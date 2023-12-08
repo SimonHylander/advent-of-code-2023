42b29531c58bf5372603bbfb5a5a504e032aae0b
Simon Hylander <hylandersimon@gmail.com>
2023-12-08 07:17:13 +0100

    5

--- typescript/src/05/05_a.ts
+++ typescript/src/05/05_a.ts
@@ -1,84 +1,94 @@
 async function run() {
-  const input = await Bun.file(new URL(`${import.meta.url}/../input.txt`)).text();
+  const input = await Bun.file(
+    new URL(`${import.meta.url}/../input.txt`)
+  ).text();
+
   const lines = input.split("\n\n");
-  const seeds = lines.shift()?.split(": ")[1].split(" ").map(Number)
+  const seeds = lines.shift()?.split(": ")[1].split(" ").map(Number);
 
-type MapElement = {
-  src: number,
-  dst: number,
-  range: number,
-}
+  type MapElement = {
+    src: number;
+    dst: number;
+    range: number;
+  };
 
-function range(line: string) {
-  const [src, dst, range] = line.split(" ")
+  type Node = {
+    src: string;
+    dst: string;
+    elements: MapElement[];
+  };
 
-  return {
-    src: +src,
-    dst: +dst,
-    range: +range
-  } as MapElement
-}
+  function range(line: string) {
+    const [dst, src, range] = line.split(" ");
 
-function parse(line: string) {
-  const filtered = line.split("\n").filter(n => n)
-  const [src, _, dst] = filtered.shift()?.split(" ")[0].split("-") || []
-  
-  return {
-    src,
-    dst,
-    elements: filtered.map(range)
-  }
-}
-
-const data = lines.map(line => parse(line))
-const map = data.reduce((acc, item) => {
-  acc[item.src] = item
-  return acc
-}, {})
-
-// console.log(map);
-
-function findNode(seedNumber: number, name: string, map: { [key: string]: { src: number, dst: number, elements: MapElement[]  } }) {
-  if (map[name] === undefined) {
-    return seedNumber
+    return {
+      src: +src,
+      dst: +dst,
+      range: +range,
+    } as MapElement;
   }
 
-  const item = map[name]
+  function parse(line: string) {
+    const filtered = line.split("\n").filter((n) => n);
+    const [src, _, dst] = filtered.shift()?.split(" ")[0].split("-") || [];
 
-  const range = item.elements.find(n => n.src <= seedNumber && seedNumber <= (n.src + n.range))
-  
-  if (range) {
-    const next = range.dst + (seedNumber - range.src)
-    return findNode(next, item.dst, map)
+    return {
+      src,
+      dst,
+      elements: filtered.map(range),
+    } as Node;
   }
 
-  return findNode(seedNumber, item.dst, map)
-}
+  type Map = {
+    [key: string]: Node;
+  };
 
-let sum = 0
-seeds?.forEach(seed => {
-  const location = findNode(seed, "seed", map)
-  console.log(seed, location);
-  
-})
+  const data = lines.map((line) => parse(line));
+  const map: Map = data.reduce((acc, item) => {
+    acc[item.src] = item;
+    return acc;
+  }, {} as Map);
 
-return
+  function findNode(seedNumber: number, name: string, map: Map) {
+    if (map[name] === undefined) {
+      return seedNumber;
+    }
+
+    const item = map[name];
+
+    // find the line that has seed number in range
+    const rangeLine = item.elements.find(
+      (n) => n.src <= seedNumber && seedNumber <= n.src + n.range
+    );
+
+    if (rangeLine) {
+      const index = seedNumber - rangeLine.src;
+      const next = rangeLine.dst + index;
+      return findNode(next, item.dst, map);
+    }
+
+    return findNode(seedNumber, item.dst, map);
+  }
+
+  let lowest = seeds?.map(seed => findNode(seed, "seed", map)).sort((a,b) => a-b);
+  console.log(lowest)
+
+  return;
 
   const oga = new Map<string, number[]>();
   const weo = {};
-  const arr = []
+  const arr = [];
 
   let categoryIndex = 0;
   Object.keys(map).forEach((key) => {
     const [srcType, dstType] = key.split("map:")[0].split("-to-");
 
-    const nomeros = {
-    };
+    const nomeros = {};
 
-    weo[srcType] = []
+    weo[srcType] = [];
 
     map[key].forEach(([dst, src, length]) => {
-      for (let i = 0; i <= src+length; i++) {
+      for (let i = 0; i <= src + length; i++) {
         nomeros[i] = i;
       }
 
@@ -90,7 +100,7 @@ return
     });
 
     Object.keys(nomeros).forEach((key) => {
-      weo[srcType].push([+key, nomeros[key]])
+      weo[srcType].push([+key, nomeros[key]]);
       // weo[categoryIndex].push([+key, nomeros[key]])
     });
 
@@ -107,7 +117,7 @@ return
     categoryIndex++;
   });
 
-  console.log(weo)
+  console.log(weo);
 
   // 35
   // console.log(seeds);
--- typescript/src/05/05_b.ts
+++ typescript/src/05/05_b.ts
@@ -1,7 +1,110 @@
 async function run() {
-  const input = await Bun.file(new URL(`${import.meta.url}/../input.txt`)).text();
-  const lines = input.split("\n");
-  console.log(lines);
+  const input = await Bun.file(
+    new URL(`${import.meta.url}/../input.txt`)
+  ).text();
+
+  const lines = input.split("\n\n");
+
+  const seeds = lines
+    .shift()
+    ?.split(": ")[1]
+    .split(" ")
+    .map(Number)
+    .reduce(
+      (acc, n, index, arr) => (
+        index % 2 === 0 ? acc.push([n, arr[index + 1]]) : acc, acc
+      ),
+      [] as number[][]
+    );
+
+  type MapElement = {
+    src: number;
+    dst: number;
+    range: number;
+  };
+
+  type Node = {
+    src: string;
+    dst: string;
+    elements: MapElement[];
+  };
+
+  function range(line: string) {
+    const [dst, src, range] = line.split(" ");
+
+    return {
+      src: +src,
+      dst: +dst,
+      range: +range,
+    } as MapElement;
+  }
+
+  function parse(line: string) {
+    const filtered = line.split("\n").filter((n) => n);
+    const [src, _, dst] = filtered.shift()?.split(" ")[0].split("-") || [];
+
+    return {
+      src,
+      dst,
+      elements: filtered.map(range),
+    } as Node;
+  }
+
+  type Map = {
+    [key: string]: Node;
+  };
+
+  const map: Map = lines
+    .map((line) => parse(line))
+    .reduce((acc, item) => {
+      acc[item.src] = item;
+      return acc;
+    }, {} as Map);
+
+  function findNode(seedNumber: number, name: string, map: Map) {
+    if (map[name] === undefined) {
+      return seedNumber;
+    }
+
+    const item = map[name];
+
+    // find the line that has seed number in range
+    const rangeLine = item.elements.find(
+      (n) => n.src <= seedNumber && seedNumber <= n.src + n.range
+    );
+
+    if (rangeLine) {
+      const index = seedNumber - rangeLine.src;
+      const next = rangeLine.dst + index;
+      return findNode(next, item.dst, map);
+    }
+
+    return findNode(seedNumber, item.dst, map);
+  }
+
+  /* let lowest = seeds
+    ?.map((seed) => findNode(seed, "seed", map))
+    .sort((a, b) => a - b); */
+
+  // console.log(seeds);
+  let tot = 0;
+  const arr: number[] = [];
+  seeds?.forEach((seedPair) => {
+    // console.log(seedPair);
+    const [seed, range] = seedPair;
+    // console.log(location);
+    for (let i = 0; i < range; i++) {
+      const location = findNode(seed + i, "seed", map);
+      arr.push(location);
+    }
+    tot += range;
+  });
+
+  arr.sort((a, b) => a - b)
+  console.log(arr);
+
+  console.log(tot);
+  // console.log(lowest);
 }
 
 run();
